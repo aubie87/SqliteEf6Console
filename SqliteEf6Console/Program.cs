4c5f7d6354ddac6c0e7e09f91266e1b814056015
Mike Grant <mgrant@hc3.io>
2021-02-08 07:55:25 -0600

    Actual working project with Sqlite and EF6.

---
+++ SqliteEf6Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteEf6Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Error loading Sqlite.Interop.dll?
+            // PM> Update-Package -reinstall System.Data.SQLite.Core
+            using (SqliteEf6Context context = new SqliteEf6Context())
+            {
+                context.People.Add(new Person { Name = "Abe", Birthday = DateTime.Now.AddYears(-20) });
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SqliteEf6Console/SqliteEf6Context.cs
@@ -0,0 +1,46 @@
+﻿using SQLite.CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common;
+using System.Data.SQLite;
+using System.Data.SQLite.EF6;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteEf6Console
+{
+    public class SQLiteConfiguration : DbConfiguration
+    {
+        public SQLiteConfiguration()
+        {
+            SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
+            SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
+            SetProviderServices("System.Data.SQLite", (DbProviderServices)SQLiteProviderFactory.Instance.GetService(typeof(DbProviderServices)));
+        }
+    }
+
+    [DbConfigurationType(typeof(SQLiteConfiguration))]
+    public class SqliteEf6Context : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //var model = modelBuilder.Build(Database.Connection);
+            //ISqlGenerator sqlGenerator = new SqliteSqlGenerator();
+            //string sql = sqlGenerator.Generate(model.StoreModel);
+            //Debug.WriteLine("OnModelCreating: " + sql);
+
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<SqliteEf6Context>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+
+            //var model = modelBuilder.Build(Database.Connection);
+            //IDatabaseCreator sqliteDatabaseCreator = new SqliteDatabaseCreator();
+            //sqliteDatabaseCreator.Create(Database, model);
+        }
+
+    }
+}
